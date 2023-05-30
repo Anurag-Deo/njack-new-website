12c413d8381ad3d22fdaa8d504ed53b6086a077b
Anuag-Deo <anuragdeo03@gmail.com>
2023-05-30 21:55:50 +0530

    Added loader and events

--- src/pages/[pageId].js
+++ src/pages/[pageId].js
@@ -1,30 +1,60 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react';
+import 'react-notion-x/src/styles.css';
+import { NotionPage } from '../components/NotionPage';
+import { NotionAPI } from 'notion-client';
+import { Footer, Header } from '@/components';
+import styles from '../styles/slug.module.css';
 
-import 'react-notion-x/src/styles.css'
-import { NotionPage } from '../components/NotionPage'
-import { NotionAPI } from 'notion-client'
-
-const notion = new NotionAPI()
+const notion = new NotionAPI();
 
 export const getStaticProps = async (context) => {
-  const pageId = (context.params.pageId) || rootNotionPageId
-  const recordMap = await notion.getPage(pageId)
+  const pageId = context.params.pageId || rootNotionPageId;
+  const recordMap = await notion.getPage(pageId);
 
   return {
     props: {
-      recordMap
+      recordMap,
     },
-    revalidate: 10
-  }
-}
+    revalidate: 10,
+  };
+};
 
 export async function getStaticPaths() {
   return {
     paths: [],
-    fallback: true
-  }
+    fallback: true,
+  };
 }
 
 export default function Page({ recordMap }) {
-  return <NotionPage recordMap={recordMap}  />
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (recordMap) {
+      setIsLoading(false);
+    }
+  }, [recordMap]);
+
+  return (
+    <>
+      <Header selected={'Departments'} />
+      {isLoading ? (
+        <div className={styles.loading}>
+        <div className={styles.opposites}>
+        <div className={`${styles.opposite} ${styles.bl}`} />
+        <div className={`${styles.opposite} ${styles.tr}`} />
+        <div className={`${styles.opposite} ${styles.br}`} />
+        <div className={`${styles.opposite} ${styles.tl}`} />
+        </div>
+      </div>
+      
+      
+      ) : (
+        <>
+          <NotionPage recordMap={recordMap} />
+          <Footer />
+        </>
+      )}
+    </>
+  );
 }
