4cce7b5cafe636cf0d95bcef2587d003d5f3d073
Anuag-Deo <anuragdeo03@gmail.com>
2023-06-25 17:34:03 +0530

    Added loder to entire website

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,37 @@
 import '@/styles/globals.css';
+import styles from '@/styles/slug.module.css';
 import { Josefin_Sans } from 'next/font/google';
+import {useRouter} from "next/router";
+import { useEffect, useState } from 'react';
+
+function Loading(){
+  const router = useRouter();
+  const [Loading, setLoading] = useState(false);
+  useEffect(()=>{
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+    const handleComplete = (url) => (url === router.asPath) && setTimeout(()=>{setLoading(false)},1500);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () =>{
+      router.events.off('routeChangeStart', handleStart); 
+    router.events.off('routeChangeComplete', handleComplete);
+    router.events.off('routeChangeError', handleComplete);
+    }
+  })
+   return Loading && (
+    <div className={styles.loading}>
+          <div className={styles.opposites}>
+            <div className={`${styles.opposite} ${styles.bl}`} />
+            <div className={`${styles.opposite} ${styles.tr}`} />
+            <div className={`${styles.opposite} ${styles.br}`} />
+            <div className={`${styles.opposite} ${styles.tl}`} />
+          </div>
+        </div>
+   )
+}
 
 const josefin_Sans = Josefin_Sans({
   style: 'normal',
@@ -11,6 +43,7 @@ const josefin_Sans = Josefin_Sans({
 export default function App({ Component, pageProps }) {
   return (
     <main className={josefin_Sans.className}>
+      <Loading />
       <Component {...pageProps} />
     </main>
   );
