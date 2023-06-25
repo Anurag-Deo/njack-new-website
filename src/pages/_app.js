42dad19dfb61ad02c3cd2c1e15c1a805a68fa1eb
Anuag-Deo <anuragdeo03@gmail.com>
2023-06-25 19:29:40 +0530

    Fixed the infinite loader issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,9 +4,10 @@ import { Josefin_Sans } from 'next/font/google';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
+
 function Loading() {
   const router = useRouter();
-  const [Loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const handleStart = (url) => url !== router.asPath && setLoading(true);
     const handleComplete = (url) =>
@@ -26,7 +27,7 @@ function Loading() {
     };
   });
   return (
-    Loading && (
+    loading && (
       <div className={styles.loading}>
         <div className={styles.opposites}>
           <div className={`${styles.opposite} ${styles.bl}`} />
@@ -47,10 +48,29 @@ const josefin_Sans = Josefin_Sans({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setLoading(true);
+    const handleComplete = (url) =>
+      url === router.asPath &&
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
+      router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+    });
   return (
     <main className={josefin_Sans.className}>
       <Loading />
-      <Component {...pageProps} />
+      {!loading && <Component {...pageProps} />}
     </main>
   );
 }
