59da20eec760d26b524f51e19add5582c0267969
Anuag-Deo <anuragdeo03@gmail.com>
2023-09-03 09:58:21 +0530

    Changed the google form for the orientation

--- src/pages/orientation.js
+++ src/pages/orientation.js
@@ -14,7 +14,7 @@ const ContactUs = () => {
                 <h1 style={{ marginBottom: '50px', textAlign: 'center' }}>ORIENTATION QUIZ FORM</h1>
                 <div className={styles.body}>
                     <iframe className={styles.panorama} width="1200" title="Panorama Viewer" scrolling="no" src="https://renderstuff.com/tools/360-panorama-web-viewer-embed/?image=https://res.cloudinary.com/dmc7njn59/image/upload/v1693666808/Frame_1_uztwuf.jpg"></iframe>
-                    <iframe style={{marginTop: "50px"}} width="1200" height="1200"  title="Google Form" src="https://forms.gle/6nexjxBvwdYt2CH69"></iframe>
+                    <iframe style={{marginTop: "50px"}} width="1200" height="1200"  title="Google Form" src="https://forms.gle/wYvnX6ryWTvpEcuK9"></iframe>
 
                 </div>
             </div>
