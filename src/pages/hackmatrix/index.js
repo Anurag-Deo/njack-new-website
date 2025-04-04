19ad891ab885a83efd4693584e8372c33a393ec6
kushal Agarwal <118652910+kushalag02@users.noreply.github.com>
2025-04-04 23:39:41 +0530

    Update index.js

--- src/pages/hackmatrix/index.js
+++ src/pages/hackmatrix/index.js
@@ -168,6 +168,7 @@ const SubmissionForm = ({ children, style }) => {
       <div className={styles.countdownContainer}>
       <img src="/hackmatrix/hackmatrix.png" alt="" className=''/>
           <>
+            <div className={styles.expired}>Results</div>
             <Link href="/hackmatrix/leaderboard">
               <button className={styles.leaderboardButton}>View Leaderboard</button>
             </Link>
